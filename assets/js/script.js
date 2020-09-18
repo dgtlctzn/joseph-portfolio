53a7853d50a76ed5fc6795318964fa48cab4332c
Joseph Perry <josephperry720@gmail.com>
2020-09-17 20:24:29 -0400

    added links to images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log("hello");
+var containerEl = $(".link");
+
+containerEl.on("mouseenter", function() {
+    $(this).children().next().removeClass("hide");
+    $(this).children().next().addClass("show");
+
+    $(this).children().first().addClass("opacity");
+})
+
+containerEl.on("mouseleave", function() {
+    $(this).children().next().removeClass("show");
+    $(this).children().next().addClass("hide");
+
+    $(this).children().first().removeClass("opacity");
+})
